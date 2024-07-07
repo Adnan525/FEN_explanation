227d11a8f25288d045366a935c1417937235dd3d
Adnan525 <adnaan525@gmail.com>
2024-07-08 04:01:48 +1000

    doing final explanation now, util done

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-def fen_to_board(fen: str) -> str:
-    """
-    Converts a FEN string into a board state
-    :param fen: fen string, e.g. 2r3k1/p4p1p/4pp2/1b6/pP1P3P/P3PN2/2rN1PP1/R3K2R w KQ - 1 20
-    :return: the board state, with empty square denoted as 1
-    """
-    rows = fen.split(' ')[0].split('/')
-    board = ""
-    for row in rows:
-        board_row = ""
-        for char in row:
-            if char.isdigit():
-                board_row += " 1" * int(char)
-            else:
-                board_row += " " + char
-        board += board_row + "\n"
-    return board
+from util import fen_to_board, get_piece_positions, count_pieces
 
-
-FEN = "1R6/p3k1p1/2p2b2/2Pn4/1BQPB3/P7/6PP/3q2K1 w - - 1 33"
-result = fen_to_board(FEN)
-print(result)
-
+FEN = "4r1k1/1pR3p1/p2pn1qp/8/PPBP4/1QP1n3/3N2PP/5RK1"
